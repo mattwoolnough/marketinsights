e287a71f06d7420266b72b1bf497b27a42bfc04f
unknown <matt@woolnough.com.au>
2016-07-18 20:35:49 +1000

    added favicon

--- app.js
+++ app.js
@@ -12,19 +12,18 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+//app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
+
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'logo32.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-//app.use(express.static(__dirname + '/public'));
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
